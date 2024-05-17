0be6b444423fb127b7f4f4ce215352935618a30f
yurifelipeYT <yuri.felipe0212@gmail.com>
2024-05-17 20:51:45 -0300

    Update problems.py
    Na parte do 'add' está errado porque, add significa +
    
    e no problema está a - b o 
     correto é a + b

--- problems.py
+++ problems.py
@@ -1,6 +1,6 @@
 def operations(a, b, operation):
     if operation == 'add':
-        return a - b
+        return a + b
     elif operation == 'subtract':
         return a * b
     elif operation == 'multiply':
